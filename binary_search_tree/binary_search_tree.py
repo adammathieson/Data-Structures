a95de2f669e3dcf6386a98691e1b58042792fdf2
Adam Mathieson <mathieson1222@gmail.com>
2019-10-17 15:24:32 -0700

    all tests passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -47,36 +47,92 @@ class BinarySearchTree:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if not self.right:
-            return self.value
-        return self.right.get_max()
+        # Recursive solution
+        # if not self.right:
+        #     return self.value
+        # return self.right.get_max()
+
+        # Iterative Solution
+        max_value = self.value
+        current = self
+
+        while current:
+            # if current value is greater than max we update it
+            if current.value > max_value:
+                max_value = current.value
+
+            current = current.right
+
+        return max_value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        cb(self.value)
+        # Recursive Solution
+        # cb(self.value)
 
-        if self.left:
-            self.left.for_each(cb)
-        if self.right:
-            self.right.for_each(cb)
+        # if self.left:
+        #     self.left.for_each(cb)
+        # if self.right:
+        #     self.right.for_each(cb)
+
+        #Iterative
+        stack = []
+        stack.append(self)
+
+        while len(stack):
+            current_node = stack.pop()
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+            cb(current_node.value)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        # values = []
+        if node:
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
+
+        
+
+            
+        
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        storage = Queue()
+        current = self
+        storage.enqueue(current)
+        while storage.len() > 0:
+            current = storage.dequeue()
+            print(current.value)
+            if current.left:
+                storage.enqueue(current.left)
+            if current.right:
+                storage.enqueue(current.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        storage = Stack()
+        current = self
+        storage.push(current)
+        while storage.len() > 0:
+            current = storage.pop()
+            print(current.value)
+            if current.left:
+                storage.push(current.left)
+            if current.right:
+                storage.push(current.right)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
@@ -88,3 +144,25 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+#Reverse linked list solution - not tested
+    # def reverse(listHead):
+    # prev = None
+    #     current = listHead
+    #     next = listHead.next
+
+    #     while current:
+    #         next = current.next
+    #         current.next = prev
+    #         prev = current
+    #         current = next
+
+    #     listhead = prev
+
+
+# test = []
+# test.append(1)
+# test.append(2)
+# test.append(3)
+
+# print('test:', test)
